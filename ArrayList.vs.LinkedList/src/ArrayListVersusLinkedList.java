7bbc0a4d4e3f0bbda0a7e20deda27988d1c9a267
Dimitri Vasiliev <Dimi7ri@users.noreply.github.com>
2016-02-14 20:17:59 +1100

    Update ArrayListVersusLinkedList.java

--- ArrayList.vs.LinkedList/src/ArrayListVersusLinkedList.java
+++ ArrayList.vs.LinkedList/src/ArrayListVersusLinkedList.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 public class ArrayListVersusLinkedList {
 
 	/*
-	 * This is a performance comparison of ArrayList and LinkedLists.
+	 * This is a execution time comparison of ArrayList and LinkedLists.
 	 * - Both collections share the same methods:
 	 * - .add(); .get(); .remove();
 	 * - Lets put them side by side.
@@ -18,52 +18,52 @@ public class ArrayListVersusLinkedList {
 			//Adding one million elements to an ArrayList.
 		System.out.println("Adding elements to our ArrayList...");
 		long startTime = System.currentTimeMillis();
-	    for (int I = 1; I <= 10000; I++){
-	    	arraylist1.add("Element number "+I);
-	     }
+	    	for(int I = 1; I <= 10000; I++){
+	    		arraylist1.add("Element number "+I);
+	     	}
 	    
-	    long stopTime = System.currentTimeMillis();
-	    long elapsedTime = stopTime - startTime;
+	    	long stopTime = System.currentTimeMillis();
+	    	long elapsedTime = stopTime - startTime;
 		System.out.println("First element of the ArrayList is: "+arraylist1.get(0)+" and Last element is: "+arraylist1.get(9999));
-	    System.out.println("Added ten thousand Strings in: "+elapsedTime+" ms");
+	    	System.out.println("Added ten thousand Strings in: "+elapsedTime+" ms");
 
 	    	//Adding one million elements to an LinkedList.
 		System.out.println("\n\nAdding elements to our LinkedList...");
 		startTime = System.currentTimeMillis();
-	    for (int I = 1; I <= 10000; I++){
-	    	linkedlist1.add("Element number "+I);
-	     }
+	    	for(int I = 1; I <= 10000; I++){
+	    		linkedlist1.add("Element number "+I);
+	     	}
 	    
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
 		System.out.println("First element of the LinkedList is: "+linkedlist1.get(0)+" and Last element is: "+linkedlist1.get(9999));
-	    System.out.println("Added ten thousand Strings in: "+elapsedTime+" ms");
+	    	System.out.println("Added ten thousand Strings in: "+elapsedTime+" ms");
 
-	    /************************** GETTING ELEMENTS ************************************/
+	    	/************************** GETTING ELEMENTS ************************************/
 			//Getting one million elements to an ArrayList.
 		System.out.println("\n\nGetting elements from our ArrayList...");
 		startTime = System.currentTimeMillis();
-	    for (int I = 0; I < 10000; I++){
-	    	arraylist1.get(I);
-	     }
+	    	for(int I = 0; I < 10000; I++){
+	    		arraylist1.get(I);
+	     	}
 	    
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("Got ten thousand Strings in: "+elapsedTime+" ms");
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("Got ten thousand Strings in: "+elapsedTime+" ms");
 
 	    	//Getting one million elements to an LinkedList.
 		System.out.println("\n\nGetting elements from our LinkedList...");
 		startTime = System.currentTimeMillis();
-	    for (int I = 0; I < 10000; I++){
-	    	linkedlist1.get(I);
-	     }
+	    	for (int I = 0; I < 10000; I++){
+	    		linkedlist1.get(I);
+	     	}
 	    
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("Got ten thousand Strings in: "+elapsedTime+" ms");
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("Got ten thousand Strings in: "+elapsedTime+" ms");
 
 
-	   /************************** DELETING ELEMENTS ************************************/
+	   	/************************** DELETING ELEMENTS ************************************/
 			//Removing one million elements to an ArrayList.
 		System.out.println("\n\nRemoving one thousand elements from our ArrayList...");
 		startTime = System.currentTimeMillis();
@@ -71,9 +71,9 @@ public class ArrayListVersusLinkedList {
 			arraylist1.remove(I);
 		}
 	    
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("Removed One thousand Elements in: "+elapsedTime+" ms");
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("Removed One thousand Elements in: "+elapsedTime+" ms");
 
 	    	//Removing one million elements to an LinkedList.
 		System.out.println("\n\nRemoving one thousand elements from our LinkedList...");
@@ -82,9 +82,9 @@ public class ArrayListVersusLinkedList {
 			linkedlist1.remove(I);
 		}
 	    
-	    stopTime = System.currentTimeMillis();
-	    elapsedTime = stopTime - startTime;
-	    System.out.println("Removed One thousand Elements: "+elapsedTime+" ms");
+	    	stopTime = System.currentTimeMillis();
+	    	elapsedTime = stopTime - startTime;
+	    	System.out.println("Removed One thousand Elements: "+elapsedTime+" ms");
 	    
 		/* 	Conclusion:
 		*   Adding elements is FASTER with LinkedList.
@@ -93,5 +93,4 @@ public class ArrayListVersusLinkedList {
 		* 	Note that my test was performed under Java version "1.8.0_72"
 		*/
 	}
-
 }
